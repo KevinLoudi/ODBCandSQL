6244a11087bd92f9f38c4157e96032dcd2419bdc
KevinLoudi <zhugengyuloudi@gmail.com>
2016-10-13 09:51:13 +0800

    More SQL method accessing an established table (self-defined sort,group,filter)

--- CreateDataBase.sql
+++ CreateDataBase.sql
@@ -1,3 +1,9 @@
+/*
+  Author: Kevin
+  Propose: Learn basic SQL method on MySQL
+  Date: 13th, Oct, 2016
+*/
+
 CREATE SCHEMA `dbstation` ;
 
 CREATE TABLE `DBStation`.`organization` (
@@ -58,3 +64,75 @@ VALUES (7, 'Muffy', 24, 'Indore', 10000.00 );
 -- show part of segment in the table
 SELECT ID, NAME, SALARY FROM CUSTOMERSINFO;
 SELECT * FROM CUSTOMERSINFO;
+
+-- select with condition
+SELECT ID, NAME, SALARY 
+FROM CUSTOMERSINFO
+WHERE SALARY > 2000;
+
+SELECT ID, NAME, SALARY 
+FROM CUSTOMERSINFO
+WHERE NAME = 'Hardik';
+
+-- with mutlipy condition, logically 'and'
+SELECT ID, NAME, SALARY 
+FROM CUSTOMERSINFO
+WHERE SALARY > 2000 AND age < 25;
+
+-- with mutlipy condition, logically 'or'
+SELECT ID, NAME, SALARY 
+FROM CUSTOMERSINFO
+WHERE SALARY > 2000 OR age < 25;
+
+-- update exist record
+SELECT * FROM CUSTOMERSINFO
+
+UPDATE CUSTOMERSINFO
+SET ADDRESS = 'Pune'
+WHERE ID = 6;
+SELECT * FROM CUSTOMERSINFO
+
+-- re setup all segement
+UPDATE CUSTOMERSINFO
+-- all address and salary will be update
+SET ADDRESS = 'Pune', SALARY = 1000.00;
+
+--delete part of the record
+DELETE FROM CUSTOMERSINFO
+WHERE ID = 6;
+
+-- select record whose salary start with '200'
+-- make comparsion
+-- '%' represent several following character and '_' following
+-- singal character
+SELECT * FROM CUSTOMERSINFO
+WHERE SALARY LIKE '200%';
+
+-- show the first 3 record
+SELECT TOP 3 * FROM CUSTOMERSINFO;
+-- the following method is similar
+SELECT * FROM CUSTOMERSINFO LIMIT 3;
+-- only work in Oracle 
+SELECT * FROM CUSTOMERSINFO WHERE ROWNUM <= 3;
+
+-- sort the record by one column or several colum combine
+SELECT * FROM CUSTOMERSINFO ORDER BY NAME, SALARY;
+
+-- group similar data set
+SELECT NAME, SUM(SALARY) FROM CUSTOMERSINFO GROUP BY NAME;
+
+-- skip the repeated record
+-- when a specific salary appear twice, 'DISTINCT'
+--   delete the extra record
+SELECT SALARY FROM CUSTOMERSINFO ORDER BY SALARY;
+SELECT DISTINCT SALARY FROM CUSTOMERSINFO ORDER BY SALARY;
+
+-- sort record in a self-defined way
+SELECT * FROM CUSTOMERSINFO
+        ORDER BY (CASE ADDRESS
+        WHEN 'DELHI'     THEN 1
+        WHEN 'BHOPAL'    THEN 2
+        WHEN 'KOTA'      THEN 3
+        WHEN 'AHMADABAD' THEN 4
+        WHEN 'MP'   THEN 5
+        ELSE 100 END) ASC, ADDRESS DESC;
